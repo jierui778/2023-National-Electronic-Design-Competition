c838046d80363aaa2355d4e9b5c440b96ae6ab15
Azlic <2311032232@qq.com>
2023-07-17 14:17:51 +0800

    merge u

--- 2023competition/项目源码/f for stm32/SYSTEM/usart/usart.c
+++ 2023competition/项目源码/f for stm32/SYSTEM/usart/usart.c
@@ -1,5 +1,13 @@
 #include "sys.h"
 #include "usart.h"	  
+#include "stm32f10x.h"                  // Device header
+#include <stdio.h>
+#include <stdarg.h>
+
+uint8_t Serial_TxPacket[8];				//FF 01 02 03 04 FE
+uint8_t Serial_RxPacket[8];
+uint8_t Serial_RxFlag;
+
 ////////////////////////////////////////////////////////////////////////////////// 	 
 //如果使用ucos,则包括下面的头文件即可.
 #if SYSTEM_SUPPORT_OS
@@ -32,31 +40,6 @@
 ////////////////////////////////////////////////////////////////////////////////// 	  
  
 
-//////////////////////////////////////////////////////////////////
-//加入以下代码,支持printf函数,而不需要选择use MicroLIB	  
-#if 1
-#pragma import(__use_no_semihosting)             
-//标准库需要的支持函数                 
-struct __FILE 
-{ 
-	int handle; 
-
-}; 
-
-FILE __stdout;       
-//定义_sys_exit()以避免使用半主机模式    
-void _sys_exit(int x) 
-{ 
-	x = x; 
-} 
-//重定义fputc函数 
-int fputc(int ch, FILE *f)
-{      
-	while((USART1->SR&0X40)==0);//循环发送,直到发送完毕   
-    USART1->DR = (u8) ch;      
-	return ch;
-}
-#endif 
 
  
  
@@ -146,3 +129,170 @@ void USART1_IRQHandler(void)                	//串口1中断服务程序
 } 
 #endif	
 
+//next is from jiangkeda
+
+
+void Serial_Init(void)
+{
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART3, ENABLE);
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);
+	
+	GPIO_InitTypeDef GPIO_InitStructure;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_10;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init(GPIOB, &GPIO_InitStructure);
+	
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU;
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_11;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init(GPIOB, &GPIO_InitStructure);
+	
+	USART_InitTypeDef USART_InitStructure;
+	USART_InitStructure.USART_BaudRate = 115200;//????????115200
+	USART_InitStructure.USART_WordLength = USART_WordLength_8b;//???8?????
+	USART_InitStructure.USART_StopBits = USART_StopBits_1;//?????
+	USART_InitStructure.USART_Parity = USART_Parity_No;//??????
+	USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;//????????
+	USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;	//????
+  
+	USART_Init(USART3, &USART_InitStructure); //?????	3
+  
+ 
+	USART_Cmd(USART3, ENABLE);                    //???? 
+	
+	//??????
+	USART_ITConfig(USART3, USART_IT_RXNE, ENABLE);//???? 
+	
+	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);
+	
+	NVIC_InitTypeDef NVIC_InitStructure;
+	NVIC_InitStructure.NVIC_IRQChannel = USART3_IRQn;
+	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority=0 ;//?????3
+	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 3;		//????3
+	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;			//IRQ????
+	NVIC_Init(&NVIC_InitStructure);	//??????????VIC???
+	
+	
+	USART_Cmd(USART3, ENABLE);
+}
+
+void Serial_SendByte(uint8_t Byte)
+{
+	USART_SendData(USART3, Byte);
+	while (USART_GetFlagStatus(USART3, USART_FLAG_TXE) == RESET);
+}
+
+void Serial_SendArray(uint8_t *Array, uint16_t Length)
+{
+	uint16_t i;
+	for (i = 0; i < Length; i ++)
+	{
+		Serial_SendByte(Array[i]);
+	}
+}
+
+void Serial_SendString(char *String)
+{
+	uint8_t i;
+	for (i = 0; String[i] != '\0'; i ++)
+	{
+		Serial_SendByte(String[i]);
+	}
+}
+
+uint32_t Serial_Pow(uint32_t X, uint32_t Y)
+{
+	uint32_t Result = 1;
+	while (Y --)
+	{
+		Result *= X;
+	}
+	return Result;
+}
+
+void Serial_SendNumber(uint32_t Number, uint8_t Length)
+{
+	uint8_t i;
+	for (i = 0; i < Length; i ++)
+	{
+		Serial_SendByte(Number / Serial_Pow(10, Length - i - 1) % 10 + '0');
+	}
+}
+
+int fputc(int ch, FILE *f)
+{
+	Serial_SendByte(ch);
+	return ch;
+}
+
+void Serial_Printf(char *format, ...)
+{
+	char String[100];
+	va_list arg;
+	va_start(arg, format);
+	vsprintf(String, format, arg);
+	va_end(arg);
+	Serial_SendString(String);
+}
+
+
+uint8_t Serial_GetRxFlag(void)
+{
+	if (Serial_RxFlag == 1)
+	{
+		Serial_RxFlag = 0;
+		return 1;
+	}
+	return 0;
+}
+
+void USART3_IRQHandler(void)
+{
+	static uint8_t RxState = 0;
+	static uint8_t pRxPacket = 0;
+	if (USART_GetITStatus(USART3, USART_IT_RXNE) == SET)
+	{
+		uint8_t RxData = USART_ReceiveData(USART3);
+		
+		if (RxState == 0)
+		{
+			if (RxData == 0xFF)
+			{
+				RxState = 1;	
+				pRxPacket = 0;
+			}
+		}
+		else if (RxState==1)
+		{
+			if (RxData == 0xFE)
+			{
+				RxState = 2;
+
+			}
+			else 
+			{
+				RxState =0;
+			}
+		}
+		else if (RxState == 2)
+		{
+			Serial_RxPacket[pRxPacket] = RxData;
+			pRxPacket ++;
+			if (pRxPacket >= 6)
+			{
+				RxState = 3;
+			}
+		}
+		else if (RxState == 3)
+		{
+			if (RxData == 0xFE)
+			{
+				RxState = 0;
+				Serial_RxFlag = 1;
+			}
+		}
+
+		USART_ClearITPendingBit(USART3, USART_IT_RXNE);
+	}
+}
