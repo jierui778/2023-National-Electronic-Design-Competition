7cdece59f8fe5c1c57d2d2f234f6f33292b382fe
Azlic <2311032232@qq.com>
2023-07-17 21:31:44 +0800

    add MPU6050

--- 2023competition/项目源码/f for stm32/SYSTEM/usart/usart.c
+++ 2023competition/项目源码/f for stm32/SYSTEM/usart/usart.c
@@ -132,50 +132,50 @@ void USART1_IRQHandler(void)                	//串口1中断服务程序
 //next is from jiangkeda
 
 
-void Serial_Init(void)
-{
-	RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART3, ENABLE);
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);
-	
-	GPIO_InitTypeDef GPIO_InitStructure;
-	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_10;
-	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-	GPIO_Init(GPIOB, &GPIO_InitStructure);
-	
-	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU;
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_11;
-	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-	GPIO_Init(GPIOB, &GPIO_InitStructure);
-	
-	USART_InitTypeDef USART_InitStructure;
-	USART_InitStructure.USART_BaudRate = 115200;//????????115200
-	USART_InitStructure.USART_WordLength = USART_WordLength_8b;//???8?????
-	USART_InitStructure.USART_StopBits = USART_StopBits_1;//?????
-	USART_InitStructure.USART_Parity = USART_Parity_No;//??????
-	USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;//????????
-	USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;	//????
-  
-	USART_Init(USART3, &USART_InitStructure); //?????	3
-  
- 
-	USART_Cmd(USART3, ENABLE);                    //???? 
-	
-	//??????
-	USART_ITConfig(USART3, USART_IT_RXNE, ENABLE);//???? 
-	
-	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);
-	
-	NVIC_InitTypeDef NVIC_InitStructure;
-	NVIC_InitStructure.NVIC_IRQChannel = USART3_IRQn;
-	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority=0 ;//?????3
-	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 3;		//????3
-	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;			//IRQ????
-	NVIC_Init(&NVIC_InitStructure);	//??????????VIC???
-	
-	
-	USART_Cmd(USART3, ENABLE);
-}
+//void Serial_Init(void)
+//{
+//	RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART3, ENABLE);
+//	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);
+//	
+//	GPIO_InitTypeDef GPIO_InitStructure;
+//	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
+//	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_10;
+//	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+//	GPIO_Init(GPIOB, &GPIO_InitStructure);
+//	
+//	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU;
+//	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_11;
+//	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+//	GPIO_Init(GPIOB, &GPIO_InitStructure);
+//	
+//	USART_InitTypeDef USART_InitStructure;
+//	USART_InitStructure.USART_BaudRate = 115200;//????????115200
+//	USART_InitStructure.USART_WordLength = USART_WordLength_8b;//???8?????
+//	USART_InitStructure.USART_StopBits = USART_StopBits_1;//?????
+//	USART_InitStructure.USART_Parity = USART_Parity_No;//??????
+//	USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;//????????
+//	USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;	//????
+//  
+//	USART_Init(USART3, &USART_InitStructure); //?????	3
+//  
+// 
+//	USART_Cmd(USART3, ENABLE);                    //???? 
+//	
+//	//??????
+//	USART_ITConfig(USART3, USART_IT_RXNE, ENABLE);//???? 
+//	
+//	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);
+//	
+//	NVIC_InitTypeDef NVIC_InitStructure;
+//	NVIC_InitStructure.NVIC_IRQChannel = USART3_IRQn;
+//	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority=0 ;//?????3
+//	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 3;		//????3
+//	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;			//IRQ????
+//	NVIC_Init(&NVIC_InitStructure);	//??????????VIC???
+//	
+//	
+//	USART_Cmd(USART3, ENABLE);
+//}
 
 void Serial_SendByte(uint8_t Byte)
 {
