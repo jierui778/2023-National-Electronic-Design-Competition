fc97ff145b2c93f38632d5555db998a456d9e937
Jierui <7584181012qq.com>
2023-07-18 10:19:33 +0800

    some change with pid

--- 2023competition/项目源码/f for stm32/HARDWARE/pid.h
+++ 2023competition/项目源码/f for stm32/HARDWARE/pid.h
@@ -1,9 +1,15 @@
 #ifndef __PID_H
 #define __PID_H
 #include "sys.h"
-
-// extern void Encoder_TIM3_Init(void);
-// extern void Encoder_TIM4_Init(void);
+typedef struct
+{
+    float kp;
+    float ki;
+    float kd;
+}PID;
+extern int Position_PID(float Encoder,float Target);
+extern int Position_PID2(float Encoder,float Target);
+extern int PositionPIDToSpeed(float Encoder,float Target);
 
 
 #endif
