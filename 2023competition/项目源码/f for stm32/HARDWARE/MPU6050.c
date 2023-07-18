5f9a351856c5130ec9742bd0aa0d60f5cb83aea8
Jierui <7584181012qq.com>
2023-07-18 14:11:24 +0800

    some change with mpu6050

--- 2023competition/项目源码/f for stm32/HARDWARE/MPU6050.c
+++ 2023competition/项目源码/f for stm32/HARDWARE/MPU6050.c
@@ -2,6 +2,7 @@
 //由于是软件模拟，取其他引脚也可以
 
 #include "stm32f10x.h"                  // Device header
+#include "MPU6050.h"
 #include "delay.h"
 
 #define MPU6050_ADDRESS		0xD0
@@ -32,19 +33,19 @@
 
 
 //IIC
-void MyI2C_W_SCL(uint8_t BitValue)
+void MPU6050_I2C_W_SCL(uint8_t BitValue)
 {
 	GPIO_WriteBit(GPIOB, GPIO_Pin_10, (BitAction)BitValue);
 	Delay_us(10);
 }
 
-void MyI2C_W_SDA(uint8_t BitValue)
+void MPU6050_I2C_W_SDA(uint8_t BitValue)
 {
 	GPIO_WriteBit(GPIOB, GPIO_Pin_11, (BitAction)BitValue);
 	Delay_us(10);
 }
 
-uint8_t MyI2C_R_SDA(void)
+uint8_t MPU6050_I2C_R_SDA(void)
 {
 	uint8_t BitValue;
 	BitValue = GPIO_ReadInputDataBit(GPIOB, GPIO_Pin_11);
@@ -52,7 +53,7 @@ uint8_t MyI2C_R_SDA(void)
 	return BitValue;
 }
 
-void MyI2C_Init(void)
+void MPU6050_I2C_Init(void)
 {
 	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);
 	
@@ -65,59 +66,59 @@ void MyI2C_Init(void)
 	GPIO_SetBits(GPIOB, GPIO_Pin_10 | GPIO_Pin_11);
 }
 
-void MyI2C_Start(void)
+void MPU6050_I2C_Start(void)
 {
-	MyI2C_W_SDA(1);
-	MyI2C_W_SCL(1);
-	MyI2C_W_SDA(0);
-	MyI2C_W_SCL(0);
+	MPU6050_I2C_W_SDA(1);
+	MPU6050_I2C_W_SCL(1);
+	MPU6050_I2C_W_SDA(0);
+	MPU6050_I2C_W_SCL(0);
 }
 
-void MyI2C_Stop(void)
+void MPU6050_I2C_Stop(void)
 {
-	MyI2C_W_SDA(0);
-	MyI2C_W_SCL(1);
-	MyI2C_W_SDA(1);
+	MPU6050_I2C_W_SDA(0);
+	MPU6050_I2C_W_SCL(1);
+	MPU6050_I2C_W_SDA(1);
 }
 
-void MyI2C_SendByte(uint8_t Byte)
+void MPU6050_I2C_SendByte(uint8_t Byte)
 {
 	uint8_t i;
 	for (i = 0; i < 8; i ++)
 	{
-		MyI2C_W_SDA(Byte & (0x80 >> i));
-		MyI2C_W_SCL(1);
-		MyI2C_W_SCL(0);
+		MPU6050_I2C_W_SDA(Byte & (0x80 >> i));
+		MPU6050_I2C_W_SCL(1);
+		MPU6050_I2C_W_SCL(0);
 	}
 }
 
-uint8_t MyI2C_ReceiveByte(void)
+uint8_t MPU6050_I2C_ReceiveByte(void)
 {
 	uint8_t i, Byte = 0x00;
-	MyI2C_W_SDA(1);
+	MPU6050_I2C_W_SDA(1);
 	for (i = 0; i < 8; i ++)
 	{
-		MyI2C_W_SCL(1);
-		if (MyI2C_R_SDA() == 1){Byte |= (0x80 >> i);}
-		MyI2C_W_SCL(0);
+		MPU6050_I2C_W_SCL(1);
+		if (MPU6050_I2C_R_SDA() == 1){Byte |= (0x80 >> i);}
+		MPU6050_I2C_W_SCL(0);
 	}
 	return Byte;
 }
 
-void MyI2C_SendAck(uint8_t AckBit)
+void MPU6050_I2C_SendAck(uint8_t AckBit)
 {
-	MyI2C_W_SDA(AckBit);
-	MyI2C_W_SCL(1);
-	MyI2C_W_SCL(0);
+	MPU6050_I2C_W_SDA(AckBit);
+	MPU6050_I2C_W_SCL(1);
+	MPU6050_I2C_W_SCL(0);
 }
 
-uint8_t MyI2C_ReceiveAck(void)
+uint8_t MPU6050_I2C_ReceiveAck(void)
 {
 	uint8_t AckBit;
-	MyI2C_W_SDA(1);
-	MyI2C_W_SCL(1);
-	AckBit = MyI2C_R_SDA();
-	MyI2C_W_SCL(0);
+	MPU6050_I2C_W_SDA(1);
+	MPU6050_I2C_W_SCL(1);
+	AckBit = MPU6050_I2C_R_SDA();
+	MPU6050_I2C_W_SCL(0);
 	return AckBit;
 }
 
@@ -128,39 +129,39 @@ uint8_t MyI2C_ReceiveAck(void)
 //MPU6050
 void MPU6050_WriteReg(uint8_t RegAddress, uint8_t Data)
 {
-	MyI2C_Start();
-	MyI2C_SendByte(MPU6050_ADDRESS);
-	MyI2C_ReceiveAck();
-	MyI2C_SendByte(RegAddress);
-	MyI2C_ReceiveAck();
-	MyI2C_SendByte(Data);
-	MyI2C_ReceiveAck();
-	MyI2C_Stop();
+	MPU6050_I2C_Start();
+	MPU6050_I2C_SendByte(MPU6050_ADDRESS);
+	MPU6050_I2C_ReceiveAck();
+	MPU6050_I2C_SendByte(RegAddress);
+	MPU6050_I2C_ReceiveAck();
+	MPU6050_I2C_SendByte(Data);
+	MPU6050_I2C_ReceiveAck();
+	MPU6050_I2C_Stop();
 }
 
 uint8_t MPU6050_ReadReg(uint8_t RegAddress)
 {
 	uint8_t Data;
 	
-	MyI2C_Start();
-	MyI2C_SendByte(MPU6050_ADDRESS);
-	MyI2C_ReceiveAck();
-	MyI2C_SendByte(RegAddress);
-	MyI2C_ReceiveAck();
+	MPU6050_I2C_Start();
+	MPU6050_I2C_SendByte(MPU6050_ADDRESS);
+	MPU6050_I2C_ReceiveAck();
+	MPU6050_I2C_SendByte(RegAddress);
+	MPU6050_I2C_ReceiveAck();
 	
-	MyI2C_Start();
-	MyI2C_SendByte(MPU6050_ADDRESS | 0x01);
-	MyI2C_ReceiveAck();
-	Data = MyI2C_ReceiveByte();
-	MyI2C_SendAck(1);
-	MyI2C_Stop();
+	MPU6050_I2C_Start();
+	MPU6050_I2C_SendByte(MPU6050_ADDRESS | 0x01);
+	MPU6050_I2C_ReceiveAck();
+	Data = MPU6050_I2C_ReceiveByte();
+	MPU6050_I2C_SendAck(1);
+	MPU6050_I2C_Stop();
 	
 	return Data;
 }
 
 void MPU6050_Init(void)
 {
-	MyI2C_Init();
+	MPU6050_I2C_Init();
 	MPU6050_WriteReg(MPU6050_PWR_MGMT_1, 0x01);
 	MPU6050_WriteReg(MPU6050_PWR_MGMT_2, 0x00);
 	MPU6050_WriteReg(MPU6050_SMPLRT_DIV, 0x09);
--- 2023competition/项目源码/f for stm32/HARDWARE/MPU6050.h
+++ 2023competition/项目源码/f for stm32/HARDWARE/MPU6050.h
@@ -1,23 +1,28 @@
+/*
+ * @Author: Jierui 7584181012qq.com
+ * @Date: 2023-07-18 10:19:54
+ * @LastEditors: Jierui 7584181012qq.com
+ * @LastEditTime: 2023-07-18 14:06:04
+ * @FilePath: \2023电赛\competition\2023competition\项目源码\f for stm32\HARDWARE\MPU6050.h
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 #ifndef __MPU6050_H
 #define __MPU6050_H
 
-
-
-
-void MyI2C_Init(void);
-void MyI2C_Start(void);
-void MyI2C_Stop(void);
-void MyI2C_SendByte(uint8_t Byte);
-uint8_t MyI2C_ReceiveByte(void);
-void MyI2C_SendAck(uint8_t AckBit);
-uint8_t MyI2C_ReceiveAck(void);
+void MPU6050_I2C_Init(void);
+void MPU6050_I2C_Start(void);
+void MPU6050_I2C_Stop(void);
+void MPU6050_I2C_SendByte(uint8_t Byte);
+uint8_t MPU6050_I2C_ReceiveByte(void);
+void MPU6050_I2C_SendAck(uint8_t AckBit);
+uint8_t MPU6050_I2C_ReceiveAck(void);
 
 void MPU6050_WriteReg(uint8_t RegAddress, uint8_t Data);
 uint8_t MPU6050_ReadReg(uint8_t RegAddress);
 
 void MPU6050_Init(void);
 uint8_t MPU6050_GetID(void);
-void MPU6050_GetData(int16_t *AccX, int16_t *AccY, int16_t *AccZ, 
-						int16_t *GyroX, int16_t *GyroY, int16_t *GyroZ);
+void MPU6050_GetData(int16_t *AccX, int16_t *AccY, int16_t *AccZ,
+					 int16_t *GyroX, int16_t *GyroY, int16_t *GyroZ);
 
 #endif
