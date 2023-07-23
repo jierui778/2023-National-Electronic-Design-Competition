f2251331198158c76581273d9f368776040e6825
Jierui <758418101@qq.com>
2023-07-23 20:25:25 +0800

    一些修改

---
+++ 2023competition/项目源码/for stm32f103zet6/HARDWARE/iic/soft_iic.c
@@ -0,0 +1,2 @@
+#include "soft_iic.h"
+
---
+++ 2023competition/项目源码/for stm32f103zet6/HARDWARE/mpu6050/mpu6050.c
@@ -0,0 +1,10 @@
+#include "mpu6050.h"
+#include "sys.h"
+
+// short short MPU6050_GetTemperature(void)
+// {
+//     uint8_t buf[2];
+//     float temp;
+//     temp=36.53+
+
+// }
